98b4e66910f2d1f7db7056ecb465e57ac993a70e
Amine0x54 <amine.tonzar@gmail.com>
2024-11-15 10:49:16 +0100

    PushNewUser

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
-console.log("test from node", Math.random(), new Date())
+const express = require('express');
+const app = express();
+const port = 3000;
+
+const users = [
+	{ id: 1, firstName: 'John', lastName: 'Doe', role: 'admin' },
+	{ id: 2, firstName: 'Jane', lastName: 'Smith', role: 'user' },
+	{ id: 3, firstName: 'Alice', lastName: 'Johnson', role: 'moderator' },
+	{ id: 4, firstName: 'Bob', lastName: 'Brown', role: 'user' },
+	{ id: 5, firstName: 'Charlie', lastName: 'Davis', role: 'admin' }
+];
+
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+        res.json(users)
+});
+
+app.listen(port, () => {
+	console.log(`Serveur en cours d'exécution sur http://localhost:${port}`);
+});
+
+
+
+app.put("/", (req, res) => {
+	res.json({
+		msg: "hello rest api ici le put",
+	})
+})
+
+app.delete("/", (req, res) => {
+	res.json({
+		msg: "hello rest api ici le delete",
+	})
+})
+
+app.post("/", (req, res) => {
+	const { firstName, lastName } = req.body
+
+    const lastId = users[users.length - 1].id
+
+    const newId = lastId + 1
+
+	const newUser = {
+		firstName,
+		lastName,
+        id:newId,
+	}
+
+	users.push(newUser)
+    
+    res.status(201).json(newUser)
+})

87d603ad51b6b77f22ba445925bbe4a84cd690a3
Amine0x54 <amine.tonzar@gmail.com>
2024-11-19 00:24:38 +0100

    final version

--- routes/users.js
+++ routes/users.js
@@ -1,18 +1,10 @@
 const express = require("express")
 const router = express.Router()
-
+const {getAllUsers}= require ("../controllers/usersControllers")
 
 const db=require("../database")
 
-router.get('/users', (req, res) => {
-    db.all('SELECT * FROM users', [], (err, rows) => {
-        if (err) {
-          res.status(500).json({ error: err.message });
-        } else {
-          res.json(rows);
-        }
-      });
-});
+router.get('/users', );
 
 router.post("/users", (req, res) => {
 const { firstName, lastName } = req.body
